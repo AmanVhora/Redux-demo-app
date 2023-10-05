69eeb0ff13a20b1ef7a6e1bd574cf8d65ee35a60
AmanVhora <amanvhora745@gmail.com>
2023-10-05 12:25:12 +0530

    Show posts list

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,1 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [
+  { id: '1', title: 'First Post!', content: 'Hello, World!' },
+  { id: '2', title: 'Second Post!', content: 'How Are You?' },
+]
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {}
+})
+
+export default postsSlice.reducer
